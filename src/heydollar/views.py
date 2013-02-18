537a6b6448d46472bb1ab3abe6f36c1614d20d63
econne01 <=eric.connelly08@gmail.com>
2013-02-17 23:15:59 -0500

    Basic working views and templates for categories and transactions

--- src/heydollar/views.py
+++ src/heydollar/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.base import TemplateView
+from heydollar.models import TransactionType, AccountType, Account, Category, Transaction 
 
 def index(request):
     test_var = 'This string is a test variable'
     context = {'var': test_var}
     return render(request, 'heydollar_index.html', context)
 
+class CategoryListView(ListView):
+    model = Category
+    
+    def get_queryset(self):
+        qs = super(CategoryListView, self).get_queryset()
+        return qs.order_by('name')
+    
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListView, self).get_context_data(**kwargs)
+        return context
+    
+class TransactionListView(ListView):
+    model = Transaction
+    
+    def get_queryset(self):
+        qs = super(TransactionListView, self).get_queryset()
+        return qs.order_by('date', 'description')
+    
+    def get_context_data(self, **kwargs):
+        context = super(TransactionListView, self).get_context_data(**kwargs)
+        return context
--- src/project/urls.py
+++ src/project/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from heydollar.views import CategoryListView, TransactionListView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -7,4 +8,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'heydollar.views.index', name='index'),
+    url(r'^categories$', CategoryListView.as_view(template_name='categories.html'), name='category_list'),
+    url(r'^history$', TransactionListView.as_view(template_name='transaction_history.html'), name='transaction_list'),
 )
