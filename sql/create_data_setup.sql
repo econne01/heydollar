c3579808fbb5314bb5b11c75c0c4252b1aa587a0
econne01 <=eric.connelly08@gmail.com>
2013-04-07 19:32:40 -0400

    Revise dummy data setup

--- sql/create_data_setup.sql
+++ sql/create_data_setup.sql
@@ -10,14 +10,14 @@ INSERT INTO account_financialInstitution (name) VALUES ('Bank of America'), ('Ch
 INSERT INTO account_accountType (name, base_sign) 
 VALUES ('checking', 1), ('saving', 1), ('credit card', -1), ('personal investment', 1), ('retirement investment', 1);
 
-INSERT INTO account_account (description, institution_id, type_id, owner_id) 
+INSERT INTO account_account (description, institution_id, type_id, owner_id, login_user, login_password) 
 VALUES 
-('BoA Checking', '1', '1', 1), ('BoA Savings', '1', '2', 1), 
-('Chase Shared Checking', '2', '1', 1), 
-('Fidelity 401k', '4', '5', 1), ('Fidelity Individual', '4', '4', 1),
-('Vanguard IRA', '5', '5', 1), ('Vanguard Cash Holdings', '5', '5', 1), ('Vanguard Personal', '5', '4', 1),
-('FIA Credit Card', '3', '3', 1),
-('HSBC Savings', '6', '2', 1);
+('BoA Checking', '1', '1', 1, '', ''), ('BoA Savings', '1', '2', 1, '', ''), 
+('Chase Shared Checking', '2', '1', 1, '', ''), 
+('Fidelity 401k', '4', '5', 1, '', ''), ('Fidelity Individual', '4', '4', 1, '', ''),
+('Vanguard IRA', '5', '5', 1, '', ''), ('Vanguard Cash Holdings', '5', '5', 1, '', ''), ('Vanguard Personal', '5', '4', 1, '', ''),
+('FIA Credit Card', '3', '3', 1, '', ''),
+('HSBC Savings', '6', '2', 1, '', '');
 
 INSERT INTO spending_category (name)
 VALUES ('Groceries'), ('Fast Food'), ('Restaurants'), ('Bars'), ('Alcohol'), ('Coffee'), ('Treats'),
