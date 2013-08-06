14a30a77ad6bd4b1e27e6266345dba0bc7f34bbb
eric.connelly <eric.connelly@essencedigital.com>
2013-08-06 18:58:47 -0400

    Setup local essence settings

--- src/project/settings/local.py
+++ src/project/settings/local.py
@@ -1,1 +1,12 @@
 from _common import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'heydollar', # Or path to database file if using sqlite3.
+        'USER': 'root',
+        'PASSWORD': 'max',
+        'HOST': '',
+        'PORT': '',
+    }
+}
