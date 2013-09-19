03a5116db3a7517d7879cb2f9f6c1809bbc0fb0b
econne01 <=eric.connelly08@gmail.com>
2013-09-19 07:36:58 -0400

    Add account list and create views

--- src/heydollar/account/urls.py
+++ src/heydollar/account/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
-from heydollar.account.views import AccountListView
+from heydollar.account.views import AccountListView, AccountCreateView
 
 urlpatterns = patterns('',
-    url(r'^accounts$', AccountListView.as_view(template_name='accounts.html'), name='account_list'),
+    url(r'^$', AccountListView.as_view(template_name='accounts.html'), name='account_list'),
+    url(r'^add$', AccountCreateView.as_view(template_name='account_form.html'), name='account_form'),
 )
--- src/heydollar/account/views.py
+++ src/heydollar/account/views.py
@@ -1,5 +1,12 @@
 from django.views.generic.list import ListView
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 from heydollar.account.models import Account
 
 class AccountListView(ListView):
     model = Account
+
+class AccountCreateView(CreateView):
+    model = Account
+    fields = ['description', 'type', 'institution']
