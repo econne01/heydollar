f11038435ba00218b0fa2d63656f9bb5b3c4987c
econne01 <=eric.connelly08@gmail.com>
2013-09-28 15:49:12 -0400

    Add list and create view FinInst. Update Nav Bar menu

--- src/heydollar/account/urls.py
+++ src/heydollar/account/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, include, url
-from heydollar.account.views import AccountListView, AccountCreateView
+from heydollar.account import views
 
 urlpatterns = patterns('',
-    url(r'^$', AccountListView.as_view(template_name='accounts.html'), name='account_list'),
-    url(r'^add$', AccountCreateView.as_view(template_name='account_form.html'), name='account_form'),
+    url(r'^$', views.AccountListView.as_view(template_name='account_list.html'), name='account_list'),
+    url(r'^add$', views.AccountCreateView.as_view(template_name='account_form.html'), name='account_form'),
+    url(r'^institution/$', views.FinancialInstitutionListView.as_view(template_name='financialinstitution_list.html'), name='financial_institution_list'),
+    url(r'^institution/add$', views.FinancialInstitutionCreateView.as_view(), name='financialinstitution_form'),
 )
--- src/heydollar/account/views.py
+++ src/heydollar/account/views.py
@@ -1,12 +1,21 @@
-from django.views.generic.list import ListView
 from django.core.urlresolvers import reverse_lazy
+from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-from heydollar.account.models import Account
+from heydollar.account.models import Account, FinancialInstitution
 
 class AccountListView(ListView):
     model = Account
 
 class AccountCreateView(CreateView):
     model = Account
+    success_url=reverse_lazy('account_list')
     fields = ['description', 'type', 'institution']
+
+class FinancialInstitutionListView(ListView):
+    model = FinancialInstitution
+
+class FinancialInstitutionCreateView(CreateView):
+    model = FinancialInstitution
+    success_url=reverse_lazy('financial_institution_list')
+    fields = ['name']
