d7eb546d14bb04a7748850a4326f2e4bf7c6663b
Eric Connelly <eric.connelly08@gmail.com>
2020-11-28 13:03:27 -0500

    Update README and add default account_owner.py template file

---
+++ config/account_owner_DO_NOT_MODIFY.py
@@ -0,0 +1,7 @@
+"""
+This file contains a mapping of account names to the name of the Person who owns the money in that account
+"""
+
+ACCOUNT_OWNER_MAP = {
+    "COMPANY 401k": "Eric Example Name",
+}
